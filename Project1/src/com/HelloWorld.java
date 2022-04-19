4f81091e3febad012404e7b5ff78030b8722a2cc
yadav-anjali <anjalijanjali99@gmail.com>
2022-04-19 13:23:36 +0530

    first commit

---
+++ Project1/src/com/HelloWorld.java
@@ -0,0 +1,20 @@
+package com;
+
+public class HelloWorld {
+	
+	static int num1 = 10;
+	static int num2 = 20;
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Helloo Woorldd");
+		
+		
+		System.out.println(HelloWorld.num1 + HelloWorld.num2);
+		
+		
+		
+	}
+
+}

95e949c1a5cee6a4e5093b31371040b6e8d4c36e
yadav-anjali <anjalijanjali99@gmail.com>
2022-04-19 13:52:12 +0530

    Second Commit

--- Project1/src/com/HelloWorld.java
+++ Project1/src/com/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Helloo Woorldd");
+		
 		
 		
 		System.out.println(HelloWorld.num1 + HelloWorld.num2);
